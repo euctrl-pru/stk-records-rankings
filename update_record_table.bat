2e6e517c206b21df55a9fecd526b6b7e398307fe
ALFARO OLIVE Oscar <oscar.alfaro@eurocontrol.int>
2026-05-08 11:02:37 +0200

    added bat file

---
+++ update_record_table.bat
@@ -0,0 +1,3 @@
+c:
+cd C:/Users/oaolive/repos/stk-records-rankings
+CMD /c C:/PROGRA~1/R/R-4.5.0/bin/Rscript.exe "C:/Users/oaolive/repos/stk-records-rankings/R/update_record_table.R"  >> "C:/Users/oaolive/repos/stk-records-rankings/update_record_table.log" 2>&1
