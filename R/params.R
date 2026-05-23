9dec8990249d86ae6c9985dd9882a79b56fc2641
ALFARO OLIVE Oscar <oscar.alfaro@eurocontrol.int>
2026-05-23 19:15:42 +0200

    parametrised colnames

--- R/params.R
+++ R/params.R
@@ -1,66 +1,39 @@
 # COLUMN NAMES ----
+common_columns <- c(
+  "PLACEHOLDER_ID",
+  "INIT_DATE_PERIOD",
+  "PLACEHOLDER_FLT",
+  "RANK",
+  "PERIOD",
+  "DAYS_PERIOD",
+  "LAST_UPDATED"
+)
+
 ## Aircraft operator ----
-colnames_ao <- c(
-  "AO_ID",
-  "INIT_DATE_PERIOD",
-  "AVG_FLT",
-  "RANK",
-  "PERIOD",
-  "DAYS_PERIOD",
-  "LAST_UPDATED"
-)
+colnames_ao <- common_columns %>%
+  gsub("PLACEHOLDER_ID", "AO_ID", .) %>%
+  gsub("PLACEHOLDER_FLT", "AVG_FLT", .)
 
-colnames_ao_grp <- c(
-  "AO_GRP_NAME",
-  "INIT_DATE_PERIOD",
-  "AVG_FLT",
-  "RANK",
-  "PERIOD",
-  "DAYS_PERIOD",
-  "LAST_UPDATED"
-)
-
+colnames_ao_grp <- common_columns %>%
+  gsub("PLACEHOLDER_ID", "AO_GRP_NAME", .) %>%
+  gsub("PLACEHOLDER_FLT", "AVG_FLT", .)
 
 ## Airport ----
-colnames_ap <- c(
-  "BK_AP_ID",
-  "INIT_DATE_PERIOD",
-  "AVG_DEP_ARR",
-  "RANK",
-  "PERIOD",
-  "DAYS_PERIOD",
-  "LAST_UPDATED"
-)
+colnames_ap <- common_columns %>%
+  gsub("PLACEHOLDER_ID", "BK_AP_ID", .) %>%
+  gsub("PLACEHOLDER_FLT", "AVG_DEP_ARR", .)
 
 ## ANSP ----
-colnames_sp <- c(
-  "ANSP_ID",
-  "INIT_DATE_PERIOD",
-  "AVG_FLT",
-  "RANK",
-  "PERIOD",
-  "DAYS_PERIOD",
-  "LAST_UPDATED"
-)
+colnames_sp <- common_columns %>%
+  gsub("ANSP_ID", "BK_AP_ID", .) %>%
+  gsub("PLACEHOLDER_FLT", "AVG_FLT", .)
 
 ## COUNTRY DAIO ----
-colnames_st_aua_daio <- c(
-  "EC_ICAO_COUNTRY_CODE",
-  "INIT_DATE_PERIOD",
-  "AVG_FLT_DAIO",
-  "RANK",
-  "PERIOD",
-  "DAYS_PERIOD",
-  "LAST_UPDATED"
-)
+colnames_st_aua_daio <- common_columns %>%
+  gsub("ANSP_ID", "EC_ICAO_COUNTRY_CODE", .) %>%
+  gsub("PLACEHOLDER_FLT", "AVG_FLT_DAIO", .)
 
 ## COUNTRY DAI ----
-colnames_st_dai <- c(
-  "EC_ICAO_COUNTRY_CODE",
-  "INIT_DATE_PERIOD",
-  "AVG_FLT_DAI",
-  "RANK",
-  "PERIOD",
-  "DAYS_PERIOD",
-  "LAST_UPDATED"
-)
+colnames_st_dai <- common_columns %>%
+  gsub("ANSP_ID", "EC_ICAO_COUNTRY_CODE", .) %>%
+  gsub("PLACEHOLDER_FLT", "AVG_FLT_DAI", .)
